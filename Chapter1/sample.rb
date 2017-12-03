437ec386e549bf43b1ed51d07b63552b4f059e9d
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-03 19:02:18 +0900

    mistake the comment-out

--- Chapter1/sample.rb
+++ Chapter1/sample.rb
@@ -3,7 +3,7 @@ class Sample
     puts 'hi'
   end
 end
-// edefsコマンドで実行する(gemコマンド)
-// 因みにgemコマンドとrakeコマンドはRubyに同封されちょる
-// →なければ"gem install rdefs"でおｋ
-// 自分はエラーでたけどぶっちゃけcatで足りてるので放置ｗｗ
+#edefsコマンドで実行する(gemコマンド)
+#因みにgemコマンドとrakeコマンドはRubyに同封されちょる
+#-> なければ"gem install rdefs"でおｋ
+#自分はエラーでたけどぶっちゃけcatで足りてるので放置ｗｗ(パスを通す時間が勿体無い)
